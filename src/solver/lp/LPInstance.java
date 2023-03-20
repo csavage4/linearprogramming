cde2e6954dcf90ba14269714285f8246e4e0941b
Colin Savage <colin_f_savage@brown.edu>
2023-03-20 16:44:49 -0400

    checking values yuh

--- src/solver/lp/LPInstance.java
+++ src/solver/lp/LPInstance.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 import java.util.Scanner;
+import java.util.function.DoublePredicate;
 
 public class LPInstance
 {
@@ -34,32 +35,89 @@ public class LPInstance
     {
       cplex = new IloCplex();
     
-       // Diet Problem from Lecture Notes
-      IloNumVar[] vars = cplex.numVarArray(2, 0, 1000, IloNumVarType.Float);
+    //    // Diet Problem from Lecture Notes
+    //   IloNumVar[] vars = cplex.numVarArray(2, 0, 1000, IloNumVarType.Float);
 
-      IloNumExpr carbs = cplex.numExpr();
-      carbs = cplex.sum(carbs, cplex.prod(100, vars[0]));
-      carbs = cplex.sum(carbs, cplex.prod(250, vars[1]));
+    //   IloNumExpr carbs = cplex.numExpr();
+    //   carbs = cplex.sum(carbs, cplex.prod(100, vars[0]));
+    //   carbs = cplex.sum(carbs, cplex.prod(250, vars[1]));
   
-      cplex.addGe(carbs, 500);
-      cplex.addGe(cplex.scalProd(new int[]{100, 50}, vars), 250);	// Fat
-      cplex.addGe(cplex.scalProd(new int[]{150, 200}, vars), 600);	// Protein
+    //   cplex.addGe(carbs, 500);
+    //   cplex.addGe(cplex.scalProd(new int[]{100, 50}, vars), 250);	// Fat
+    //   cplex.addGe(cplex.scalProd(new int[]{150, 200}, vars), 600);	// Protein
 
-      // Objective function 
-      cplex.addMinimize(cplex.scalProd(new int[]{25, 15}, vars));
+    //   // Objective function 
+    //   cplex.addMinimize(cplex.scalProd(new int[]{25, 15}, vars));
 
-      if(cplex.solve())
-      {
-        objectiveValue = Math.ceil(cplex.getObjValue());
+    //   if(cplex.solve())
+    //   {
+    //     objectiveValue = Math.ceil(cplex.getObjValue());
 		
-        System.out.println("Meat:  " + cplex.getValue(vars[0]));
-        System.out.println("Bread:  " + cplex.getValue(vars[1]));
-        System.out.println("Objective value: " + cplex.getObjValue());
-      }
-      else
-      {
-        System.out.println("No Solution found!");
-      }
+    //     System.out.println("Meat:  " + cplex.getValue(vars[0]));
+    //     System.out.println("Bread:  " + cplex.getValue(vars[1]));
+    //     System.out.println("Objective value: " + cplex.getObjValue());
+    //   }
+    //   else
+    //   {
+    //     System.out.println("No Solution found!");
+    //   }
+
+        IloNumVar[] numVehicles = cplex.numVarArray(this.numFacilities, 0, numMaxVehiclePerFacility, IloNumVarType.Float);
+
+        IloNumVar[] open = cplex.numVarArray(this.numFacilities, 0, 1, IloNumVarType.Float);
+        IloNumVar[][] facilUsed = new IloNumVar[numCustomers][numFacilities];
+        for(int i=0; i<this.numCustomers; i++){
+            facilUsed[i] = cplex.numVarArray(this.numFacilities,0,1,IloNumVarType.Float);
+        }
+        for(int i=0; i<this.numFacilities; i++){
+            IloNumVar[] distPerCust = cplex.numVarArray(this.numCustomers, 0, Integer.MAX_VALUE, IloNumVarType.Float);
+            IloNumVar[] amtPerCust = cplex.numVarArray(this.numCustomers, 0, Integer.MAX_VALUE, IloNumVarType.Float);
+            for(int j = 0; j<this.numCustomers; j++){
+                cplex.addEq(distPerCust[j], cplex.prod(facilUsed[j][i], distanceCF[j][i]));
+                cplex.addEq(amtPerCust[j], cplex.prod(demandC[j], facilUsed[j][i]));
+            }
+            cplex.addLe(cplex.sum(amtPerCust), capacityF[i]);
+            cplex.addLe(cplex.sum(distPerCust), cplex.prod(numVehicles[i], truckDistLimit));
+            cplex.addEq(cplex.prod(open[i],capacityF[i]), cplex.sum(amtPerCust));
+        }
+
+        for(int i = 0; i<numCustomers; i++){
+            cplex.addEq(cplex.sum(facilUsed[i]),1);
+        }
+
+        IloNumExpr allocCost = cplex.numExpr();
+        IloNumExpr openingCost = cplex.numExpr();
+        IloNumExpr vehicleCost = cplex.numExpr();
+        IloNumExpr truckUsage = cplex.numExpr();
+
+        IloNumVar[][] scaledAlloc = new IloNumVar[numCustomers][numFacilities];
+        for(int i=0; i<this.numCustomers; i++){
+            scaledAlloc[i] = cplex.numVarArray(this.numFacilities,0,Integer.MAX_VALUE,IloNumVarType.Float);
+            for(int j= 0; j<this.numFacilities; j++){
+                cplex.addEq(scaledAlloc[i][j], cplex.prod(facilUsed[i][j],allocCostCF[i][j]));
+            }
+        }
+        IloNumVar[] scaledOpen = cplex.numVarArray(this.numFacilities, 0, Integer.MAX_VALUE, IloNumVarType.Float);
+        for(int i = 0; i<numFacilities; i++){
+            cplex.addEq(scaledOpen[i], cplex.prod(open[i],this.openingCostF[i]));
+        }
+
+        IloNumVar[] flattened = cplex.numVarArray(this.numCustomers, 0, Integer.MAX_VALUE, IloNumVarType.Float);
+        for(int i = 0; i<numCustomers; i++){
+            cplex.addEq(flattened[i], cplex.sum(scaledAlloc[i]));
+        } 
+
+        allocCost = cplex.sum(flattened);
+        openingCost = cplex.sum(scaledOpen);
+        vehicleCost = cplex.prod(cplex.sum(numVehicles), this.truckUsageCost);
+        cplex.addMinimize(cplex.sum(cplex.sum(allocCost, openingCost), vehicleCost));
+
+        if(cplex.solve())
+       {
+         objectiveValue = Math.ceil(cplex.getObjValue());
+		
+         System.out.println("Objective value: " + cplex.getObjValue());
+       }
     }
     catch(IloException e)
     {
